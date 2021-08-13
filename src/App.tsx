81ba6d6e53f545662b717d3f1724c8ce1f6d811f
arun69 <arunabhawasthi@gmail.com>
2021-08-13 13:40:30 +0530

    Search Bar with animated placeholder implemented. Parallax background added. Applied left and right margins in Navbar component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import imageHome from './assets/4853433.jpg';
 import './App.css';
 import NavBar from './components/Navbar/NavBar'
 import SearchBox from './components/Section-1/SearchBox'
@@ -6,6 +7,7 @@ import SearchBox from './components/Section-1/SearchBox'
 function App() {
   return (
     <div className="App">
+        <img id="paraImg" src={imageHome} alt="Background"/>
         <NavBar />
         <SearchBox />
     </div>
--- src/components/Section-1/SearchBox.tsx
+++ src/components/Section-1/SearchBox.tsx
@@ -1,15 +1,38 @@
-import React, {Component} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import useTypewriter from "react-typewriter-hook";
+import searchIcon from './srch.png'
+import './searchBox.css'
 
-class SearchBox extends Component<any, any>{
+const placeholderStrings = [
+    "Operating system books",
+    "4th semester books",
+    "Engineering drawing"
+]
+let index = 0;
 
-
-    render() {
-        return (
-            <div className="searchbox">
-                <input type="text" id="Search" placeholder="Search..."/>
-            </div>
-        )
-    }
+function SearchBox() {
+    const [Holder, setHolder] = useState("Search...");
+    const intervalRef = useRef({});
+    const name = useTypewriter(Holder);
+    useEffect(
+        () => {
+            intervalRef.current = setInterval(() => {
+                // index = index + 1 > 2 ? 0 : ++index + 1;
+                index = (index > 2) ? 0 : ++index;
+                setHolder(placeholderStrings[index]);
+            }, 6000);
+            return function clear() {
+                clearInterval(intervalRef.current as NodeJS.Timeout);
+            };
+        },
+        [Holder]
+    )
+    return (
+        <div className="searchbox">
+            <input type="text" placeholder={(name ? name : "")}/>
+            <button className="searchButton"><img src={searchIcon} alt="Click to search" id="searchIcon"/></button>
+        </div>
+    )
 }
 
 
